bfb4419e788531b04cbac091c52173746a203433
Hasanessaad <hasancayedessaad@gmail.com>
2022-11-26 20:06:33 -0300

    the first commit

---
+++ lojinha-atividade/interaction.js
@@ -0,0 +1,75 @@
+let ITENS_LOJA = [
+    {
+        nome:"Bola da Copa",
+        preco:86.30,
+        url_img:"../img/bola_copa.webp",
+        descricao:"Bola utilizada na Copa do Mundo, idela para utilizar em campo e colecionar"
+    },
+    {
+        nome:"Carrinho da Hot Wheels",
+        preco:5.80,
+        url_img:"../img/carro_hotwheels.jpeg",
+        descricao:"Carrinho para dar de presentes para crianças ou colecionar"
+    },
+    {
+        nome:"Chinelo",
+        preco:19.99,
+        url_img:"../img/chinelo.webp",
+        descricao:"Chinelo para se utilizar em qualquer lugar"
+    },
+    {
+        nome:"Escorredor de Louça",
+        preco:15.20,
+        url_img:"../img/escorredor_louca (2).webp",
+        descricao:"Utilizando para ajudar no processo de secagem dos utensilios"
+    },
+    {
+        nome:"Espada Medieval",
+        preco:200.18,
+        url_img:"../img/espada_medieval.png",
+        descricao:"Utilizada em batalhar para neutralizar o inimigo ou como item de decoração"
+    },
+    {
+        nome:"Guitarra",
+        preco:1980.99,
+        url_img:"../img/guitarra.webp",
+        descricao:"Otima para utilizar em bandas de hard rock"
+    },
+    {
+        nome:"Toalha de Banho",
+        preco:9.99,
+        url_img:"../img/toalha_banho.webp",
+        descricao:"Boa para se secar apos o banho"
+    },
+    {
+        nome:"Vinho",
+        preco:59.90,
+        url_img:"../img/vinho.webp",
+        descricao:"Vinho que harmoniza muito bem com carnes vermelhas "
+    }
+]
+
+for(let i=0; i <  ITENS_LOJA.length ;i++){
+
+    let allfather = document.getElementById("allfather");
+    
+    let box = document.createElement('div');
+    let img = document.createElement('img');
+    let nomee = document.createElement('p');
+    let price = document.createElement('p');
+    let description = document.createElement('p');
+    
+    img.src = ITENS_LOJA[i].url_img;
+    nomee.innerText = ITENS_LOJA[i].nome;
+    price.innerText = ITENS_LOJA[i].preco;
+    description.innerText = ITENS_LOJA[i].descricao;
+
+    allfather.appendChild(box);
+    box.appendChild(img);
+    box.appendChild(nomee);
+    box.appendChild(price);
+    box.appendChild(description);
+    
+}
+
+
